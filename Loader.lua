2cc77c1465bebfdd630ffecdd9450d20eff42eed
Hebebebe <abdulghani1010roblox@gmail.com>
2026-07-16 23:24:56 +0500

    Update script URLs to use .luau files

--- Loader.lua
+++ Loader.lua
@@ -1,5 +1,5 @@
 if game.PlaceId == 78515283254292 then
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/ABDULGHANI1010/Animal-Hospital/refs/heads/main/Lobby.lua"))()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/ABDULGHANI1010/Animal-Hospital/refs/heads/main/Lobby.luau"))()
 else
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/ABDULGHANI1010/Animal-Hospital/refs/heads/main/Main.lua"))()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/ABDULGHANI1010/Animal-Hospital/refs/heads/main/Main.luau"))()
 end
