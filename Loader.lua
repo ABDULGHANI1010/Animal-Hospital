5cdd18bb99138471e2ca32e8c6ca0a48d3dc0cb1
Hebebebe <abdulghani1010roblox@gmail.com>
2026-07-16 16:13:31 +0500

    Create Loader.lua

---
+++ Loader.lua
@@ -0,0 +1,5 @@
+if game.PlaceId == 78515283254292 then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/ABDULGHANI1010/Animal-Hospital/refs/heads/main/Lobby.lua"))()
+else
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/ABDULGHANI1010/Animal-Hospital/refs/heads/main/Main.lua"))()
+end
